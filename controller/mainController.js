b10184f7ee425150e151477c95ef675e85f5e781
Rameez <rameesmohd789@gmail.com>
2023-11-10 11:38:19 +0530

    code clean up in addData func

--- controller/mainController.js
+++ controller/mainController.js
@@ -18,8 +18,6 @@ const fetchData =async(req,res)=>{
 
 const addData=async(req,res)=>{
     try {
-        console.log(req.body);
-        console.log(req.files);
         const {name,address,contact} = req.body
         let imageURL;
         if(req.files.image.length){
@@ -57,61 +55,18 @@ const addData=async(req,res)=>{
                 }
             }
         )
-
     } catch (error) {
         console.log(error);
         res.status(500).json({})
     }
 }
 
-// const updateData=async(req,res)=>{
-//     try {
-//         const obj = req.body
-//         console.log(obj)
-//         let imageURL;
-//         if(req.files.length){
-//             const image = req.files.image[0]
-//             if(image){
-//                 const upload = await cloudinary.uploader.upload(image.path)
-//                 imageURL = upload.secure_url;
-//                 obj.image = imageURL
-//                 fs.unlinkSync(image.path)
-//             }
-//         }
-
-//         db.query('UPDATE restaurantTable SET ? WHERE id = ?',[obj,obj.id],(error,result)=>{
-//             if(error){
-//                 return res.status(500).json({message : 'Error while updating!!'})
-//             }else{
-//                 console.log(result);
-//                 const insertedId = result.insertId;
-//                 db.query(
-//                     "SELECT * FROM restaurantTable WHERE id = ?",
-//                     [insertedId],
-//                     (selectError, selectResult) => {
-//                     if (selectError) {
-//                         console.log('Error retrieving inserted data:', selectError);
-//                     } else {
-//                         const insertedData = selectResult[0];
-//                         console.log('Inserted data:', insertedData);
-//                         res.status(200).json({data : insertedData,message:'Updated successfully!!'})
-//                     }
-//                     }
-//                 )
-//                 return res.status(200).json({message : 'Updated successfully!!'})
-//             }
-//         })        
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
 const updateData = async (req, res) => {
     try {
         const obj = req.body;
         let imageURL;
-
-        if (req.files.length) {
+        
+        if(req.files.image){
             const image = req.files.image[0];
             if (image) {
                 const upload = await cloudinary.uploader.upload(image.path);
@@ -120,14 +75,11 @@ const updateData = async (req, res) => {
                 fs.unlinkSync(image.path);
             }
         }
-
         db.query('UPDATE restaurantTable SET ? WHERE id = ?', [obj, obj.id], (error, result) => {
             if (error) {
                 return res.status(500).json({ message: 'Error while updating!!', error: error });
             }
-
             const insertedId = result.insertId;
-            console.log(result);
             db.query(
                 "SELECT * FROM restaurantTable WHERE id = ?",
                 [insertedId],
@@ -135,11 +87,10 @@ const updateData = async (req, res) => {
                     if (selectError) {
                         return res.status(500).json({ message: 'Error retrieving inserted data', error: selectError });
                     }
-
                     const insertedData = selectResult[0];
                     return res.status(200).json({ data: insertedData, message: 'Updated successfully!!' });
                 }
-            );
+            )
         });
     } catch (error) {
         console.log(error);
